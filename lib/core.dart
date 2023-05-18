1e436bf89bd8bb8f3ac5cac21c19530e38a77782
Samandar Rajabboyev <rajabboyevsamandar22@gmail.com>
2023-05-18 18:24:37 +0500

    interceptor deleted

--- lib/core.dart
+++ lib/core.dart
@@ -7,7 +7,6 @@ export 'extensions/empty_padding.dart';
 export 'extensions/navigation.dart';
 export 'resources/base_repository.dart';
 export 'resources/data_state.dart';
-export 'resources/token_interceptor.dart';
 export 'resources/use_case.dart';
 export 'router/route_transitions.dart';
 export 'services/services.dart';
